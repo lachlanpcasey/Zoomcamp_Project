8169d2888887777e4001fafd8b077ba9b913a1c2
lachlanpcasey@gmail.com <lachlanpcasey@gmail.com>
2023-03-27 03:44:44 +0000

    adds descriptions, as well as a core model

--- macros/distinct_by_date.sql
+++ macros/distinct_by_date.sql
@@ -1,5 +1,12 @@
 {% macro distinct_by_date(table) %}
-SELECT *
+SELECT 
+date,
+low as daily_price_low,
+high as daily_price_high,
+open as daily_price_open,
+close as daily_price_close,
+volume_from as crypto_volume,
+volume_to as usd_volume
 FROM (
   SELECT *,
     ROW_NUMBER() OVER (PARTITION BY date ORDER BY date) as row_num
---
+++ models/core/core_crypto_open_prices.sql
@@ -0,0 +1,40 @@
+{{ config(materialized='view', schema='data_core') }}
+
+with btc as (
+    select *,
+    'BTC' as crypto
+    from {{ ref('stg_btc')}}
+)
+, eth as (
+    select *,
+    'ETH' as crypto
+    from {{ ref('stg_eth')}}
+)
+, ens as (
+    select *,
+    'ENS' as crypto
+    from {{ ref('stg_ens')}}
+)
+, ltc as (
+    select *,
+    'LTC' as crypto
+    from {{ ref('stg_ltc')}}
+)
+, xrp as (
+    select *,
+    'XRP' as crypto
+    from {{ ref('stg_xrp')}}
+)
+
+select 
+    btc.date,
+    btc.daily_price_open as btc_daily_price_open,
+    xrp.daily_price_open as xrp_daily_price_open,
+    eth.daily_price_open as eth_daily_price_open,
+    ens.daily_price_open as ens_daily_price_open,
+    ltc.daily_price_open as ltc_daily_price_open
+from btc
+left join eth on eth.date = btc.date
+left join xrp on xrp.date = btc.date
+left join ltc on ltc.date = btc.date
+left join ens on ens.date = btc.date
