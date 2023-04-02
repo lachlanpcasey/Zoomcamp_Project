f8f8dd05b8582aa6f335cdc6b061172332a828c2
Lachie <122522521+lachlanpcasey@users.noreply.github.com>
2023-04-02 21:27:20 +1000

    Update config.py

--- project/config.py
+++ project/config.py
@@ -6,13 +6,18 @@ gcs_bucket_name = ''
 prefect_cloud_api_key = ''
 #Input String 
 prefect_cloud_dbt_block_name = ''
+#Input String
+dbt_api_key = ''
+#Input Integer
+dbt_account_id =
 #Input Integer 
-prefect_cloud_job_id = 
+dbt_job_id = 
 #Input List
 prefect_crypto_list_for_etl = []
 #Input String. Name of crypto folder within bucket. EG crypto_data
 crypto_folder = ''
 #Input String. Should be the name of your BigQuery dataset ID.
 dataset_id = ''
-#Input string. Path to your google cloud service account json.
-creds = ''
+#Service account json file predefined. Copy and paste your service account into creds.json
+creds = 'creds.json'
+
